90f09c87ad3542f8c2ccdc386fcc92a259aca56e
slzhao <slzhao@wisedu.com>
2021-02-26 20:26:21 +0800

    改为通过applicationContext获取

--- demo-web/src/main/java/com/example/demo/controller/ApiController.java
+++ demo-web/src/main/java/com/example/demo/controller/ApiController.java
@@ -2,20 +2,31 @@ package com.example.demo.controller;
 
 import com.example.demo.models.UserBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ApiController {
+public class ApiController implements ApplicationContextAware {
 
-    @Autowired
+//    @Autowired
     ApiAdapter adapter;
+
+    ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext context) {
+        this.applicationContext = context;
+    }
 
     @RequestMapping(value = "/db/config",
             produces = { "application/json" },
             method = RequestMethod.GET)
     UserBean getDbConfigData() {
+        // 这里报错，NoSuchBeanDefinitionException: No qualifying bean of type 'com.example.demo.controller.ApiAdapter' available]
+        this.adapter = applicationContext.getBean(ApiAdapter.class);
         UserBean user = adapter.getData();
         return user;
     }
